33d0187eec367925906fd32d983d839112e4feb0
Quentin Pré <quentin.pre@epita.fr>
2013-11-11 16:53:49 +0100

    now authenticating successfully

--- RSConnector.rb
+++ RSConnector.rb
@@ -33,10 +33,9 @@ class RSConnector < EM::Connection
       @clientIP   = input[3]
       @clientPort = input[4]
       
-      message = "auth_ag ext_user none none\n"
-      
-      puts "client << #{message}"
-      send_data message
+      message = "auth_ag ext_user none none"
+
+      send_message message
     elsif (input[0] == 'rep')
       responseHandler input
     end
@@ -46,12 +45,18 @@ class RSConnector < EM::Connection
     if (rep[1] == '002')
       puts "authenticating"
       message = "ext_user_log #{RSConfig.instance.login} #{replyHash} #{RSConfig.instance.location} RubySoul"
-      puts message
-      send_data(message)
+      send_message message
     end
   end
   
+  def send_message message
+    message = "#{message}\n"
+    puts "client << #{message}"
+    send_data message
+  end
+  
   def replyHash
-    Digest::MD5.hexdigest("#{@hash}-#{@clientIP}/#{@clientPort}#{RSConfig.instance.location}")
+    puts "#{@hash}-#{@clientIP}/#{@clientPort}#{RSConfig.instance.location}"
+    Digest::MD5.hexdigest "#{@hash}-#{@clientIP}/#{@clientPort}#{RSConfig.instance.password}"
   end
 end
